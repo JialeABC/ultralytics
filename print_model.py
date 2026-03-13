4d92a63ee431a8782d2a0d67b608776864e61919
JialeABC <lijiale24a@nudt.edu.cn>
2026-03-13 09:45:04 +0800

    2026_3_13

--- print_model.py
+++ print_model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 直接加载模型
-model = YOLO('D:/Deeplearning_code/yolov8/ultralytics/runs/detect/train8/weights/best.pt')
+model = YOLO('D:/Deeplearning_code/yolov8/ultralytics/runs/detect/best.pt')
 
 # 打印模型信息（包括层数、参数数量、形状等）
 model.info()
